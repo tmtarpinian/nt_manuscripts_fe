439f3d815ce2c57bb8ac453cdcd6df9edf96d2d3
ferrisbueller66 <tmtarpinian@gmail.com>
2021-07-23 11:05:40 -0400

    debug references vuex module

--- src/views/References.vue
+++ src/views/References.vue
@@ -2,15 +2,24 @@
     <div>
         <h1>References</h1>
         <ul>
-            <li v-for="reference in referenceList" :key="reference.id">
-                {{reference.book}}
-            </li>
+          <reference-details
+            v-for="reference in referenceList"
+            :key="reference.id"
+            :book="reference.book"
+            :chapter="reference.chapter"
+            :verse="reference.verse"
+          ></reference-details>
         </ul>
     </div>
 </template>
 
 <script>
+import ReferenceDetails from '../components/ReferenceDetails.vue';
+
 export default {
+  components: {
+    ReferenceDetails,
+  },
   computed: {
     referenceList() {
       return this.$store.getters['references/references'];
